55a430c956be0f95407720b91091ed20c348fc1c
WeTheStudy <wethestudy.wts@gmail.com>
2023-12-29 14:44:38 +0800

    build gold 001

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/fonts/Heebo/Heebo-VariableFont_wght.ttf';
 import './index.css';
 import { MemberstackProvider } from '@memberstack/react';
 import ErrorBoundary from './components/alert/Error.js';
 import Tree from './Tree.js';
-import treeCheck from './treeCheck.js';
-import database from './database/tree-json.js';
-import { performTreeCheck } from './devSettings.js';
+// import treeCheck from './treeCheck.js';
+// import database from './database/tree-json.js';
+// import { performTreeCheck } from './devSettings.js';
 
 const App = () => {
-  useEffect(()=>{
-    if(performTreeCheck) treeCheck(database)
-  }, [])
+  // useEffect(()=>{
+  //   if(performTreeCheck) {
+  //     console.log("TREE CHECK PERFORMED")
+  //     treeCheck(database)
+  //   }
+  // }, [])
 
   return(
     <ErrorBoundary>
